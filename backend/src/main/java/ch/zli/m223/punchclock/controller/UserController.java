b7bc61d5aad9958ef6fe8d52c2dd45763029e739
GeneTv <contact@genetv.net>
2021-09-29 00:26:35 +0200

    Things seem to be back to normal. Issue still here :(

--- backend/src/main/java/ch/zli/m223/punchclock/controller/UserController.java
+++ backend/src/main/java/ch/zli/m223/punchclock/controller/UserController.java
@@ -1,5 +1,45 @@
 package ch.zli.m223.punchclock.controller;
 
-public class UserController {
+import ch.zli.m223.punchclock.domain.Entry;
+import ch.zli.m223.punchclock.domain.User;
+import ch.zli.m223.punchclock.service.EntryService;
+import ch.zli.m223.punchclock.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/entries")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@Valid @RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable long id) {
+        userService.deleteUser(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public User updateUser(@Valid @RequestBody User user) {
+        return userService.updateUser(user);
+    }
 }
