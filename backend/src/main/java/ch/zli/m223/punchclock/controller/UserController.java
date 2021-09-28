96f0c5564dcc62cc41507979e17720d310929155
GeneTv <contact@genetv.net>
2021-09-29 00:29:42 +0200

    We don't talk about this one

--- backend/src/main/java/ch/zli/m223/punchclock/controller/UserController.java
+++ backend/src/main/java/ch/zli/m223/punchclock/controller/UserController.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/entries")
+@RequestMapping("/users")
 public class UserController {
     private UserService userService;
 
