277930c952af26b26ecd9a9c56132cad35514dd0
GeneTv <contact@genetv.net>
2021-09-28 15:44:53 +0200

    Added routes

--- client/src/router.js
+++ client/src/router.js
@@ -1,22 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import securityService from './services/security'
+import securityService from '@/services/security'
 
-import Home from '../views/Home.vue'
+import Categories from '@/views/Categories.vue'
+import Entries from '@/views/Entries.vue'
+import Teams from '@/views/Teams.vue'
+import Users from '@/views/Users.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Entries',
+    component: Entries,
+    meta: {
+      authRequired: 'user'
+    }
+  },{
+    path: '/admin/users',
+    name: 'Users',
+    component: Users,
+    meta: {
+      authRequired: 'admin'
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/admin/teams',
+    name: 'Teams',
+    component: Teams,
+    meta: {
+      authRequired: 'admin'
+    }
+  },{
+    path: '/admin/categories',
+    name: 'Categories',
+    component: Categories,
+    meta: {
+      authRequired: 'admin'
+    }
+  },{
+    path: '/login',
+    name: 'Login',
+    component: Categories,
+    meta: {
+      authRequired: 'admin'
+    }
+  },{
+    path: '/register',
+    name: 'Register',
+    component: Categories,
+    meta: {
+      authRequired: 'admin'
     }
   }
 ]
