0b0389f166e0b20491aa51697b8faaf5e29d4239
GeneTv <contact@genetv.net>
2021-09-29 10:23:26 +0200

    Implemented navbar (more or less)

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <ul class="navbar__nav">
+      <li><router-link :to="item.path" v-for="item in items" :class="{ '--active': item.active }" :key="item.title" class="nav__item">{{ item.title }}</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import { routes } from '@/router'
+export default {
+  name: 'Nav',
+  computed: {
+    items() {
+      const userRole = this.$store.getters.userRole;
+      let routeItems = routes.filter( route => route.meta.inNav && route.meta.authRequired.length == 0 ? true : route.meta.authRequired.includes(userRole));
+
+      const currentRoute = this.$route;
+
+      routeItems = routeItems.map( route => ({
+        path: route.path,
+        title: route.name,
+        active: currentRoute.path == route.path
+      }))
+
+      return routeItems;
+    }
+  }
+}
+</script>
