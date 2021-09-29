02f9b6318b9f57779f73036252a7e2b1d4c3a959
GeneTv <contact@genetv.net>
2021-09-29 12:57:13 +0200

    Added message for empty entries list & a create button

--- client/src/views/Entries.vue
+++ client/src/views/Entries.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="wrapper">
+    <div class="controls">
+      <button class="button --action-edit">Create entry</button>
+    </div>
     <div class="list">
+      <div v-if="entries.length === 0">
+        <h3>No entries available</h3>
+        <p>Click on "Create entry" to add one.</p>
+      </div>
+      
       <div class="list__entry" v-for="entry in entries" :key="entry.id">
         <label class="entry__label">Check-in</label>
         <p>{{ new Date(Date.parse(entry.checkIn)).toLocaleString() }}</p>
@@ -21,7 +29,6 @@ export default {
   },
   computed: {
     entries() {
-      console.log(this.entries)
       return this.$store.state.entries;
     }
   },
