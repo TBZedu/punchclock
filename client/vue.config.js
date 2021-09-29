cf87d48bb1e0417602b5e416012189a2ca21e475
GeneTv <contact@genetv.net>
2021-09-29 08:25:11 +0200

    Configured proxy to backend

--- client/vue.config.js
+++ client/vue.config.js
@@ -11,7 +11,8 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8081',
+        pathRewrite: {'^/api': '/'},
         changeOrigin: true
       }
     }
